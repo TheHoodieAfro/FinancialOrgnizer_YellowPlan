6fd218ff6500514f12d5959cfa70755761e41bc1
Cristian Sanchez <crisey937@gmail.com>
2022-04-27 13:35:37 -0500

    Added virtual enviroment + Added basic flask function + Added requirements.txt

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'hellold'
+
 class Distribution:
     '''Class depicting the plan money distributions'''
 
-    def __init__(self, percentage, value=0):
+    def __init__(self, percentage, total_month, total=0):
         if (percentage <= 0 and percentage > 100):
             self.percentage = percentage
         else:
             raise Exception('The percentage for a distribution cannot be 0 or below, or over 100')
 
-        self.value=value
+        self.total=total
+
+        self.total_month = total_month

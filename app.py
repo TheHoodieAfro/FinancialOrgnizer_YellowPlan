35e38a9f3b93dfd82f9c7b2f270fada21474969c
Cristian Sanchez <crisey937@gmail.com>
2022-04-27 22:57:53 -0500

    General changes

--- app.py
+++ app.py
@@ -9,12 +9,20 @@ def hello():
 class Distribution:
     '''Class depicting the plan money distributions'''
 
-    def __init__(self, percentage, total_month, total=0):
+    def __init__(self, percentage, name, total_month, total=0):
         if (percentage <= 0 and percentage > 100):
             self.percentage = percentage
         else:
             raise Exception('The percentage for a distribution cannot be 0 or below, or over 100')
 
-        self.total=total
+        self.name = name
 
         self.total_month = total_month
+
+        self.total=total
+
+class Location:
+    '''Class depicting the location of the money'''
+
+    def __init__(self, name):
+        self.name = name
